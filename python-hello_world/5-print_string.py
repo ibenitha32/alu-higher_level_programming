7f66e00647eb0f4059eb1e15e994538379a45a41
Benitha <b.iradukund1@alustudent.com>
2023-02-07 10:41:29 -0800

    The first mandatory tasks

---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/bin/python3
+str = "Textbook Tech"
+print("{}{}{}".format(str, str, str))
+print("{}".format(str[:9]))
---
+++ python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+str1 = "Unplugged"
+str2 = "Reality"
+str1 = str1 + " " + str2
+print("Welcome to the {}!".format(str1))
