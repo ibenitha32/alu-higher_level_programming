6eefc6e3d94c2bf93da80c303d643050385b5bf3
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-02-07 21:27:01 +0200

    Update 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Unplugged"
-str2 = "Reality"
-str1 = str1 + " " + str2
-print("Welcome to the {}!".format(str1))
+str1 = "Holberton"
+str2 = "School"
+print(f"Welcome to {str1} + {str2}!")
