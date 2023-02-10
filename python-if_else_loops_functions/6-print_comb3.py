b8d130211e04ac5be09ff42ef7d4790c37c75dce
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-02-10 03:20:35 +0200

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if(i is 8 and j is 9):
-            print("{}".format(str(i) + str(j)))
-        elif(j > i):
-            print("{}".format(str(i) + str(j)) + ", ", end="")
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i < 8:
+            print("{:d}{:d}".format(i, j), end=", ")
+        else:
+            print("{:d}{:d}".format(i, j))
