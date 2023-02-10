132f8d9e6cf1097fe6867d7850a6678a59c426e6
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-02-10 02:39:18 +0200

    task 9

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        last = number % 10
+    else:
+        last = (number * -1) % 10
+    print("{:d}".format(last), end="")
+    return(last)
