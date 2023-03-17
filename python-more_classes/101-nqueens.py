9fc505c0ec27ee80bcfd37042f4338f8b743c78a
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-03-17 12:59:26 +0200

    please work
    Second edit

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
+"""
+N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard.
+"""
+
 import sys
 
-def is_valid(board, row, col):
-    for i in range(row):
-        if board[i] == col or \
-           board[i] - i == col - row or \
-           board[i] + i == col + row:
+
+def is_safe_pos(possibilty, col):
+    """Check if it is possible to put the queen"""
+    for i in range(col):
+        if possibilty[i] == possibilty[col] or abs(possibilty[i] - possibilty[col]) == col - i:
             return False
     return True
 
-def nqueens(board, row, n):
-    if row == n:
-        print(board)
-        return
-    for col in range(n):
-        if is_valid(board, row, col):
-            board[row] = col
-            nqueens(board, row + 1, n)
+
+def nqueens(possibilty, col, n):
+    """Recursive function to place queens on the chess board"""
+    count = 0
+    if col == n:
+        return 1
+    for i in range(n):
+        possibilty[col] = i
+        if is_safe_pos(possibilty, col):
+            count += nqueens(possibilty, col + 1, n)
+    return count
+
 
 if __name__ == "__main__":
+    # Verify the input
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
@@ -30,5 +39,12 @@ if __name__ == "__main__":
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    board = [-1] * n
-    nqueens(board, 0, n)
+
+    # Initialize an array to hold positions of queens
+    possibilty = [0 for i in range(n)]
+
+    # Calculate the number of possible solutions
+    count = nqueens(possibilty, 0, n)
+
+    # Print the number of solutions
+    print(count)
