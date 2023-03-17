6bb0b9d76d17954d285404711907142bd140a90f
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-03-17 14:10:19 +0200

    What, what?

--- python-more_classes/101-nqueens.py
+++ python-more_classes/101-nqueens.py
@@ -1,69 +1,34 @@
 #!/usr/bin/python3
 
-
-import sys
-
-
-def printBoard(board):
-    if any(1 in x for x in board):
-        print([[idx, board[idx].index(1)] for idx, val in enumerate(board)])
-
-
-def isSafe(row, square, chessboard, N, diag):
-    if chessboard[row][square]:
-        return False
-    if square - diag >= 0 and chessboard[row][square - diag]:
-        return False
-    if square + diag < (N) and chessboard[row][square + diag]:
-        return False
-    if row == 0:
-        return True
-    return isSafe(row - 1, square, chessboard, N, diag + 1)
-
-
-def placeSquare(row, position, chessboard, N):
-    for square in range(position, N):
-        if 1 in chessboard[row]:
-            return 0
-        if not isSafe(row - 1, square, chessboard, N, 1):
-            continue
-        chessboard[row][square] = 1
-        return
-    return 1
-
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-
-N = sys.argv[1]
-
-if not str.isdigit(N):
-    print("N must be a number")
-    sys.exit(1)
-
-N = int(N)
-
-if N < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-queen = 0
-
-while queen != N:
-    chessboard = [[0 for x in range(N)] for x in range(N)]
-    chessboard[0][queen] = 1
-    position = 0
-    row = 1
-    while row < N:
-        if placeSquare(row, position, chessboard, N):
-            row -= 1
-            position = chessboard[row].index(1)
-            chessboard[row][position] = 0
-            position += 1
-            if not row:
-                break
-        else:
-            row += 1
-            position = 0
-    printBoard(chessboard)
-    queen += 1
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set ()
+        posDiag = set() # (r + c)
+        negDiag = set() # (r + c)
+        
+        res = []
+        board = [["."] * n for i in range(n)]
+        
+        def backtrack(r):
+            if r == n:
+               copy = ["".join(row) for row in board]
+               res.append(copy)
+                return
+            for i in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in ned Diag:
+                    continue
+                    
+                col.add(c)
+                posDiag.add(r + c)
+                nedDiag.add(r - c)
+                board[r][c] = "Q"
+                
+                backtrack(r + 1)
+                
+                col.add(c)
+                posDiag.add(r + c)
+                nedDiag.add(r - c)
+                board[r][c] = "."
+           backtrack(0)
+           return res
+        
