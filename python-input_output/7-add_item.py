f2614545a48ca0b238f8ee8bcf3c74fed5a3f866
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-03-24 20:33:34 +0200

    mblnkdvc

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""script that adds all arguments to a Python list."""
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
