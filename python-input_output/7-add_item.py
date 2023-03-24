666c21e50b5117ec0ed840b87f6d87097b0ca1cd
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-03-24 20:42:01 +0200

    please

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list, and then save them to a file
-"""
+"""script that adds all arguments to a Python list."""
 
 import sys
-import os.path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = 'add_item.json'
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-# check if file exists, if not create an empty list
-if os.path.isfile(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
-
-# add arguments to list
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-# save list to file
-save_to_json_file(my_list, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
