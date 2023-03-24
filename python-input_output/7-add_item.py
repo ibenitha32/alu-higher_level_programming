6b8b1897c08fbb18e223b00b82e69485bd5aed7f
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-03-24 20:37:51 +0200

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        items = []
+        items = ["Holberton"]
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
