5ab49c3775d4cc4f303825d3202e35d36558074b
Benitha <b.iradukund1@alustudent.com>
2023-03-24 11:49:13 -0700

    kjkhg

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
-"""saves object as json string dump"""
-import json
+"""script that adds all arguments to a Python list."""
 
+import sys
 
-def save_to_json_file(my_obj, filename):
-    """actually does it"""
-    with open(filename, mode='w', encoding='utf-8') as f:
-        f.write(json.dumps(my_obj))
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
