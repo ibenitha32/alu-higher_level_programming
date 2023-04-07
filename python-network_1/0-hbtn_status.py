48d267b8ab9f236ec1871797580b76844118e409
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-04-07 20:54:58 +0200

    last adjustment

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""A script that 
-- fetches https://alu-intranet.hbtn.io/status.
-- uses urlib package
-"""
+""" Python script that fetches https://alu-intranet.hbtn.io/status """
+import urllib.request
 
 
 if __name__ == '__main__':
