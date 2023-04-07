36f7a918d06ac579569d4ae2f82b58cfe71cad6c
ibenitha32 <b.iradukund1@alstudent.com>
2023-04-07 17:56:36 +0000

    Adjustments

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of the response (if the status code is 200)
-curl -s -o /dev/null -w "%{http_code}" $1 | [ $(cat) == "200" ] && curl -s $1
+# sends GET req to URL and display response body
+curl -sfL "$1" -X GET
