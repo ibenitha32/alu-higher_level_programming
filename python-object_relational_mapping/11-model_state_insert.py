daf09b374c4c3a4f01b0c10bf94efdefea3e4af7
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-04-24 09:52:07 +0200

    11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""script that adds the State object
-“Louisiana” to the database hbtn_0e_6_usa
-"""
-
+""" adds the State object "Louisiana" to the database"""
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -14,11 +11,9 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    session = Session()
     Base.metadata.create_all(engine)
-
-    state = State(name="Louisiana")
-    session.add(state)
-    session.commit()
-    print(state.id)
-    session.close()
+    with Session() as session:
+        state = State(name="Louisiana")
+        session.add(state)
+        session.commit()
+        print(state.id)
