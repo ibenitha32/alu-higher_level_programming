a6152dc6e4a22c357e1eaf75705d2519a91a1064
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-04-24 09:50:46 +0200

    MS

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''class definition of a state'''
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+Base = declarative_base()
+
+
+class State(Base):
+    '''class defination of a table'''
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
