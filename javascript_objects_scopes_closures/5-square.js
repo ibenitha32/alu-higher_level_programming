c74f7a591bba75619aff8cfe15a97a574d508b30
Benitha Iradukunda <116553054+ibenitha32@users.noreply.github.com>
2023-06-16 23:47:40 +0200

    Update 5-square.js

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-const Base = require('./5-square');
+const Rectangle = require('./4-rectangle');
 
-class Square extends Base {
-  charPrint (c) {
-    for (let i = 0; i < this.height; i++) {
-      console.log((c || 'X').repeat(this.width));
-    }
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 }
 
